8050b071f98aa01414f83892169aa57992f360b6
Joseph Kariuki <joehene@gmail.com>
2024-02-19 15:11:58 +0300

    Created views

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,8 +29,10 @@ def create_app(test_config=None):
     @app.route("/hello")
     def hello():
         return "Hello, World!"
-    
-    from . import db
+
+    from . import db, auth
+
     db.init_app(app)
+    app.register_blueprint(auth.bp)
 
     return app
